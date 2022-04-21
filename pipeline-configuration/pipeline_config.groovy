627bfc1dfae311721fefc32f7b9dd6f2ec8bd9ad
future <future.wei@technet-asia.com>
2022-04-21 17:28:00 +0800

    update

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,4 +1,22 @@
 libraries{
-    gradle
+    maven
     sonarqube
+    ansible
+}
+
+stages{
+    continuous_integration{
+        build
+        static_code_analysis
+    }
+}
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
 }
